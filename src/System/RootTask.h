eb8c2aac5b4625cfb0f2c675f7bb282c04a66476
Aubrey Taylor <atmatm6000@outlook.com>
2023-08-02 16:45:45 -0700

    Fix compilation, waiting for open-ead/sead#130

--- src/System/RootTask.h
+++ src/System/RootTask.h
@@ -1,22 +1,22 @@
 #pragma once
 
 #include <gfx/seadDrawContext.h>
+#include <hostio/seadHostIONode.h>
 
 class GameSystem;
 
-// TODO: Implement sead::Task, currently missing from sead headers
-class RootTask {
+// Waiting for open-ead/sead#130
+class RootTask /*: public sead::Task*/ {
 private:
-    // TODO: Remove once RootTask implements sead::Task
-    char taskImplDetails[0x200];
+    char task_details[0x200];
     GameSystem* mGameSystem;
     sead::hostio::Node* mHostIoNode;
     sead::DrawContext* mDrawContext;
 public:
     RootTask();
     ~RootTask();
-    void enter() override;
-    void draw() override;
-    void calc() override;
-    void prepare() override;
+    void enter()/* override*/;
+    void draw()/* override*/;
+    void calc()/* override*/;
+    void prepare()/* override*/;
 };
