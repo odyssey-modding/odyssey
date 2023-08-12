ddc1dfbf82b0b277c235b51a7a34a643a98f3a30
GRAnimated <devin@nhscan.com>
2023-08-11 21:33:27 -0400

    Separate shields data from progress json

--- tools/progress.py
+++ tools/progress.py
@@ -12,41 +12,44 @@ progress_output = subprocess.check_output(["python3", "tools/common/progress.py"
 # Parse the CSV output
 csv_fields = progress_output.decode("utf-8").strip().split(",")
 
-# Calculate percentages
 num_total = int(csv_fields[3])
-code_size_total = int(csv_fields[4])
 matching_count = int(csv_fields[5])
-equivalent_count = int(csv_fields[7])
-non_matching_minor_count = int(csv_fields[9])
+non_matching_minor_count = int(csv_fields[7])
+non_matching_major_count = int(csv_fields[9])
 
 matching_percentage = (matching_count / num_total) * 100
-equivalent_percentage = (equivalent_count / num_total) * 100
 non_matching_minor_percentage = (non_matching_minor_count / num_total) * 100
+non_matching_major_percentage = (non_matching_major_count / num_total) * 100
 
 progress_data = {
-    "version": int(csv_fields[0]),
-    "timestamp": int(csv_fields[1]),
-    "git_hash": csv_fields[2],
-    "num_total": num_total,
-    "code_size_total": code_size_total,
     "matching": {
-        "count": matching_count,
-        "percentage": matching_percentage,
-        "code_size": int(csv_fields[6])
-    },
-    "equivalent": {
-        "count": equivalent_count,
-        "percentage": equivalent_percentage,
-        "code_size": int(csv_fields[8])
+        "label": "matching",
+        "message": f"{matching_percentage:.5f}%",
     },
     "non_matching_minor": {
-        "count": non_matching_minor_count,
-        "percentage": non_matching_minor_percentage,
-        "code_size": int(csv_fields[10])
+        "label": "non-matching (minor issues)",
+        "message": f"{non_matching_minor_percentage:.5f}%",
+    },
+    "non_matching_major": {
+        "label": "non-matching (major issues)",
+        "message": f"{non_matching_major_percentage:.5f}%",
     }
+}
+
+shields_data = {
+    "schemaVersion": 1,
+    "label": "decompiled",
+    "message": f"{matching_percentage:.3f}%",
+    "color": "blue",
+    "cacheSeconds": 3600,
+    "style": "flat"
 }
 
 with open('data/progress.json', 'w') as json_file:
     json.dump(progress_data, json_file, indent=4)
 
+with open('data/shields.json', 'w') as json_file:
+    json.dump(shields_data, json_file, indent=4)
+
 print("Progress data saved to 'data/progress.json'")
+print("Shields data saved to 'data/shields.json'")
